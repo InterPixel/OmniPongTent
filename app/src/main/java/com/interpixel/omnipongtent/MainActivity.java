f44d894b3c056ebd41738f08f792f94282c25c32
Renziera <stanleyheryanto@gmail.com>
2016-11-12 23:09:32 +0700

    :q
    B:::::wq
    A
    D
    C
    D

--- app/src/main/java/com/interpixel/omnipongtent/MainActivity.java
+++ app/src/main/java/com/interpixel/omnipongtent/MainActivity.java
@@ -1,13 +1,27 @@
 package com.interpixel.omnipongtent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "asdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Get executed when button pressed
+    public void doSomething(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
